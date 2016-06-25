082a2f3d2ef080f47e0d05abe0d21495305b13f7
yaqien378 <yaqien378@gmail.com>
2016-06-25 11:49:29 +0700

    Informasi
    Perbaikan informasi.

--- application/controllers/Informasi.php
+++ application/controllers/Informasi.php
@@ -17,20 +17,31 @@ class Informasi extends CI_Controller
 
 		// info directory
 		$files = directory_map("./assets/global/info/");
+
 		$info_arr = array();
-		foreach ($files as $file) {
-			// baca file
-			$file_value = read_file("./assets/global/info/".$file);
-			$arr_value = explode("|[]|", $file_value);
-			if(count($arr_value) > 0) {
-				$arr_file = array(
-						"judul"	=> $arr_value[0],
-						"tanggal" => $arr_value[1],
-						"isi" => $arr_value[2],
-						"filename" => $file
-					);
-				array_push($info_arr, $arr_file);
+		if($files != null) {
+			foreach ($files as $file) {
+				// baca file
+				$file_value = read_file("./assets/global/info/".$file);
+				$arr_value = explode("|[]|", $file_value);
+				if(count($arr_value) > 0) {
+					$arr_file = array(
+							"judul"	=> $arr_value[0],
+							"tanggal" => $arr_value[1],
+							"isi" => $arr_value[2],
+							"filename" => $file
+						);
+					array_push($info_arr, $arr_file);
+				}
 			}
+		} else {
+			$arr_file = array(
+					"judul"	=> "Informasi",
+					"tanggal" => date("d/m/Y"),
+					"isi" => "Tidak ada informasi untuk saat ini.",
+					"filename" => null
+				);
+			array_push($info_arr, $arr_file);
 		}
 		$data['informasi'] = $info_arr;
 
